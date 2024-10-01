f0eccfd78fa22f2369afe04d55b7e81a9738856d
Talha Baig <Talha@superiorgroup.com.au>
2024-10-01 11:47:53 +1000

    removed service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 {
-    if (env('APP_ENV') !== 'production') {
-        URL::forceScheme('http');
-    }
+    // if (env('APP_ENV') !== 'production') {
+    //     URL::forceScheme('http');
+    // }
 }
 
     
